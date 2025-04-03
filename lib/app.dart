151c66c2f745aad28011866f75dc40888b2b1e3e
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-04-04 00:58:09 +0500

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spacetomic/theme/theme.dart';
 
 
 class MyApp extends StatelessWidget {
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Spacetomic',
+      theme: spacetomicTheme,
       home: Home(),
     );
   }
