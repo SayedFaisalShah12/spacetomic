87db41f8b8d2f0beabc55a83ffbfbbe3046c6715
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-04-04 01:17:59 +0500

    update

---
+++ lib/core/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextTheme spacetomicTextTheme = TextTheme(
+  displayLarge: GoogleFonts.orbitron(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white),
+  displayMedium: GoogleFonts.orbitron(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white),
+  displaySmall: GoogleFonts.orbitron(fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white70),
+  titleLarge: GoogleFonts.orbitron(fontSize: 22, fontWeight: FontWeight.w600, color: Colors.deepPurpleAccent),
+  titleMedium: GoogleFonts.orbitron(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.white),
+  bodyLarge: GoogleFonts.orbitron(fontSize: 16, color: Colors.white70),
+  bodyMedium: GoogleFonts.orbitron(fontSize: 14, color: Colors.white60),
+);
