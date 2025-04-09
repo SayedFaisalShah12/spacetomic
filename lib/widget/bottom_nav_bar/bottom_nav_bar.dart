01403dbb100603b9412d6ebca24573d1c9f8144c
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-04-10 00:55:39 +0500

    update

--- lib/widget/bottom_nav_bar/bottom_nav_bar.dart
+++ lib/widget/bottom_nav_bar/bottom_nav_bar.dart
@@ -2,36 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import '../../logic/bottom_nav_bar/navigation_bloc.dart';
-import '../../logic/bottom_nav_bar/navigation_event.dart';
 import '../../logic/bottom_nav_bar/navigation_state.dart';
 
-class CustomBottomNavBar extends StatelessWidget {
+
+class CustomNavBar extends StatelessWidget {
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    Text('Home', style: optionStyle),
+    Text('Likes', style: optionStyle),
+    Text('Search', style: optionStyle),
+    Text('Profile', style: optionStyle),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<NavigationBloc, NavigationState>(
       builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          child: GNav(
-            selectedIndex: state.selectedIndex,
-            onTabChange: (index) {
-              context.read<NavigationBloc>().add(NavigateToTab(index));
-            },
-            backgroundColor: Colors.black,
-            color: Colors.white,
-            activeColor: Colors.white,
-            tabBackgroundColor: Colors.deepPurple.shade700,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            gap: 8,
-            tabs: const [
-              GButton(icon: Icons.home, text: 'Home'),
-              GButton(icon: Icons.image, text: 'Gallery'),
-              GButton(icon: Icons.search, text: 'Search'),
-              GButton(icon: Icons.settings, text: 'Settings'),
-            ],
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            elevation: 20,
+            title: const Text('GoogleNavBar with BLoC'),
+          ),
+          body: Center(
+            child: _widgetOptions[state.selectedIndex],
+          ),
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 20,
+                  color: Colors.black.withOpacity(.1),
+                )
+              ],
+            ),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+                child: GNav(
+                  rippleColor: Colors.grey[300]!,
+                  hoverColor: Colors.grey[100]!,
+                  gap: 8,
+                  activeColor: Colors.black,
+                  iconSize: 24,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  duration: Duration(milliseconds: 400),
+                  tabBackgroundColor: Colors.grey[100]!,
+                  color: Colors.black,
+                  tabs: [
+                     GButton(icon: Icons.home, text: 'Home'),
+                     GButton(icon: Icons.heart, text: 'Likes'),
+                     GButton(icon: Icons.search, text: 'Search'),
+                     GButton(icon: Icons.person_2_outlined, text: 'Profile'),
+                  ],
+                  selectedIndex: state.selectedIndex,
+                  onTabChange: (index) {
+                    context.read<NavigationBloc>().add(ChangeTab(index));
+                  },
+                ),
+              ),
+            ),
           ),
         );
       },
     );
   }
 }
+
