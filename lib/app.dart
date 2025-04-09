dbbd453d8dd3a11fdb9ed040d791fbdb31157eba
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-04-10 00:48:08 +0500

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:spacetomic/presentation/splash_screen/splash_screen.dart';
+import 'logic/bottom_nav_bar/navigation_bloc.dart';
+import 'logic/bottom_nav_bar/navigation_event.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Spacetomic',
-      home: const SplashScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NavigationBloc>(
+          create: (context) => NavigationBloc()..add(NavigateToTab(0)),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Spacetomic',
+        home: const SplashScreen(),
+      ),
     );
   }
 }
