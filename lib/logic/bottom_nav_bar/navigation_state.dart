4c1dd69db6cc0b4ea8edaee92b7d4353f5c7e4ca
Sayed Faisal Shah <142516641+SayedFaisalShah12@users.noreply.github.com>
2025-04-10 00:35:07 +0500

    update

--- lib/logic/bottom_nav_bar/navigation_state.dart
+++ lib/logic/bottom_nav_bar/navigation_state.dart
@@ -1,7 +1,4 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class NavigationCubit extends Cubit<int> {
-  NavigationCubit() : super(0);
-
-  void selectTab(int index) => emit(index);
+class NavigationState {
+  final int selectedIndex;
+  NavigationState(this.selectedIndex);
 }
